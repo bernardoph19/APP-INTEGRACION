1df24bb369997a37a85c251b6879d2ecf8427545
Berna <bernardoph19@gmail.com>
2021-03-30 00:12:32 -0500

    cambios nuevos

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -7,12 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+slides: {img: string, titulo: string} [] = [
+
+  {
+    img: 'assets/img/bienvenido.svg',
+    titulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur neque, ipsam distinctio inventore magni at quae quisquam qui dignissimos magnam eos? Nisi tempore recusandae deserunt reiciendis incidunt exercitationem odio culpa.'
+  },
+
+  {
+    img: 'assets/img/estadist.svg ',
+    titulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur neque, ipsam distinctio inventore magni at quae quisquam qui dignissimos magnam eos? Nisi tempore recusandae deserunt reiciendis incidunt exercitationem odio culpa.'
+  },
+
+  {
+    img: 'assets/img/repor.svg',
+    titulo: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur neque, ipsam distinctio inventore magni at quae quisquam qui dignissimos magnam eos? Nisi tempore recusandae deserunt reiciendis incidunt exercitationem odio culpa.'
+  },
+];
+
+
+  constructor( private navCtrl:NavController) { }
   slideOpts = {
     initialSlide: 1,
-    speed: 400
+    speed: 400,
   };
   ngOnInit() {
+  }
+
+  omitir(){
+    this.navCtrl.navigateRoot ('/dashboard');
   }
 
 }
