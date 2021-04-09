8f633416971f08a9f5a8f28227ebc450106be2d0
Bernardo <bernardoph19@gmail.com>
2021-04-09 10:55:52 -0500

    cambios en  splash screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full'
+  // },
+
   { path:  'login',                loadChildren: () => import  ('./auth/login/login.module')                                 .then( m => m.LoginPageModule)     },
   { path:  'inicio',               loadChildren: () => import  ('./pages/inicio/inicio.module')                              .then( m => m.InicioPageModule)    },
   { path:  'menu-principal',       loadChildren: () => import  ('./shared/menu-principal/menu-principal.module')             .then( m => m.MenuPrincipalPageModule) },
@@ -15,7 +16,10 @@ const routes: Routes = [
   { path: 'micuenta',              loadChildren: () => import('./pages/configuraciones/cuenta/micuenta/micuenta.module').then( m => m.MicuentaPageModule) },
   { path: 'editar-nombre',         loadChildren: () => import('./pages/configuraciones/cuenta/editar-nombre/editar-nombre.module').then( m => m.EditarNombrePageModule) },
   { path: 'listado-cliente',       loadChildren: () => import('./pages/reporte/report-admin/listado-cliente/listado-cliente.module').then( m => m.ListadoClientePageModule) },
-  
+  {
+    path: 'splash',
+    loadChildren: () => import('./pages/splash/splash.module').then( m => m.SplashPageModule)
+  },  
 
 
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform,
+    private statusBar: StatusBar,
+    private router: Router
+  ) {
+
+  }
+
+
+  iniciarApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.router.navigateByUrl('splash');
+    });
+  }
 }
