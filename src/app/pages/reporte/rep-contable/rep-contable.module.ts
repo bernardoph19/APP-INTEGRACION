8fa36ec92602d6e9b5ae88c428a1502f519b4ea0
Bernardo <bernardoph19@gmail.com>
2021-04-13 10:59:20 -0500

    reporte contable terminado

--- src/app/pages/reporte/rep-contable/rep-contable.module.ts
+++ src/app/pages/reporte/rep-contable/rep-contable.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { RepContablePageRoutingModule } from './rep-contable-routing.module';
 
 import { RepContablePage } from './rep-contable.page';
+import { DemoMaterialModule } from 'src/app/module.material';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RepContablePageRoutingModule
+    RepContablePageRoutingModule,
+    DemoMaterialModule
   ],
   declarations: [RepContablePage]
 })
