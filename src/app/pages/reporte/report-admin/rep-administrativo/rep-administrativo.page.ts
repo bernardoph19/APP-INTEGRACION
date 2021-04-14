8895322a8aa5755dc91af34bec4fcfc4ba2d8baf
Daniel Mantilla Condor <dmantilla@innovated.pe>
2021-04-14 16:12:33 -0500

    add

--- src/app/pages/reporte/report-admin/rep-administrativo/rep-administrativo.page.ts
+++ src/app/pages/reporte/report-admin/rep-administrativo/rep-administrativo.page.ts
@@ -113,7 +113,6 @@ export class RepAdministrativoPage implements OnInit {
 
   }
 
-
   async Mostrar_cpe( list : any [] ){
     list.forEach( el=>{ el.isChecked = false; })
     const modal = await this.modalEditNombRe.create({
