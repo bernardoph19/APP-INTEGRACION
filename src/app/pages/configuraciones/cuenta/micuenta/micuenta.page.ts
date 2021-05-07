a4310c2eaec07993b8d0516290cd1790f80c01e8
Bernardo <bernardoph19@gmail.com>
2021-05-07 13:33:14 -0500

    cambios nuevos en  mi cuenta

--- src/app/pages/configuraciones/cuenta/micuenta/micuenta.page.ts
+++ src/app/pages/configuraciones/cuenta/micuenta/micuenta.page.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { CambiarContrasenaPage } from '../cambiar-contrasena/cambiar-contrasena.page';
-import { EditarNombrePage } from '../editar-nombre/editar-nombre.page';
-import { EditarcorreoPage } from '../editarcorreo/editarcorreo.page';
-import { EditarnumeroPage } from '../editarnumero/editarnumero.page';
+
 
 @Component({
   selector: 'app-micuenta',
@@ -14,50 +12,11 @@ export class MicuentaPage implements OnInit {
 
   constructor(private modal: ModalController) {}
 
-  // Modal editar Nombre
-  async editarNombre() {
-    const modal = await this.modal.create({
-      component: EditarNombrePage,
-      componentProps: {
-        // nombre: 'Bernardo ',
-        // pais: 'Peru',
-      },
-    });
-    await modal.present();
-    const { data } = await modal.onDidDismiss();
-    console.log('retorno con datos', data);
-  }
+ 
 
+  editar: boolean;
 
-  // modal editar correo
-  async editarcorreo() {
-    const modal = await this.modal.create({
-      component: EditarcorreoPage,
-      componentProps: {
-        // nombre: 'Bernardo ',
-        // pais: 'Peru',
-      },
-    });
-    await modal.present();
-    const { data } = await modal.onDidDismiss();
-    console.log('retorno con daots', data);
-  }
-
-
-  // modal editar Numero
-  async editarNumero() {
-    const modal = await this.modal.create({
-      component: EditarnumeroPage,
-      componentProps: {
-        // nombre: 'Bernardo ',
-        // pais: 'Peru',
-      },
-    });
-    
-    await modal.present();
-    const { data } = await modal.onDidDismiss();
-    console.log('retorno con daots', data);
-  }
+ 
 
    //Modal Cambiar Contraseña
    async cambiarContrasena() {
